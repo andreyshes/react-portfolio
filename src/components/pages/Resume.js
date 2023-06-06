3dd9a2803894beb894ac8bda652cd72cde68d1ff
Andrey <ashesto121@gmail.com>
2023-06-05 19:01:39 -0700

    final commit

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -32,28 +32,28 @@ export default function Resume() {
 			<h2 style={styles.h2}>Resume</h2>
 			<h3 style={styles.h3}>Technical Skills</h3>
 			<p style={styles.p}>
-				<h4>Front-End</h4>
+				<h4 className="text-5xl">Front-End</h4>
 				<ul>
-					<li>HTML</li>
-					<li>CSS</li>
-					<li>Javascript</li>
-					<li>JQuery</li>
-					<li>Bootstrap</li>
-					<li>React</li>
-					<li>Responsive Design</li>
+					<li className="list-disc">HTML</li>
+					<li className="list-disc">CSS</li>
+					<li className="list-disc	">Javascript</li>
+					<li className="list-disc	">JQuery</li>
+					<li className="list-disc	">Bootstrap</li>
+					<li className="list-disc	">React</li>
+					<li className="list-disc	">Responsive Design</li>
 				</ul>
 			</p>
 			<p style={styles.p}>
-				<h4>Back-End</h4>
+				<h4 className="text-5xl">Back-End</h4>
 				<ul>
-					<li>MySQL</li>
-					<li>Sequelize</li>
-					<li>MongoDB</li>
-					<li>Mongoose</li>
-					<li>Express</li>
-					<li>Node</li>
-					<li>APIs</li>
-					<li>GraphQL</li>
+					<li className="list-disc">MySQL</li>
+					<li className="list-disc">Sequelize</li>
+					<li className="list-disc">MongoDB</li>
+					<li className="list-disc	">Mongoose</li>
+					<li className="list-disc	">Express</li>
+					<li className="list-disc	">Node</li>
+					<li className="list-disc	">APIs</li>
+					<li className="list-disc	">GraphQL</li>
 				</ul>
 			</p>
 		</div>
